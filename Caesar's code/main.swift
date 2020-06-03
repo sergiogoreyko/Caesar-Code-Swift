e57a62c168ad1b81b911164793e51642087e1a84
sergiogoreyko <sergei_goreiko@mail.ru>
2020-06-03 11:05:22 +0300

    Initial Commit

---
+++ Caesar's code/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Caesar's code
+//
+//  Created by Сергей Горейко on 03/06/2020.
+//  Copyright © 2020 Сергей Горейко. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
